9777ed0858b7b424a600316f6ecf8ad46b041a9b
scennmieayam <scendy17@gmail.com>
2025-11-29 16:52:52 +0700

    tugas pertemuan 9 - CRUD FLASK MONGODB

---
+++ Pertemuan 9/crud_flask_mongodb/app.py
@@ -0,0 +1,94 @@
+import os
+from flask import Flask, render_template, request, redirect, url_for, send_from_directory
+from flask_bootstrap import Bootstrap
+from pymongo import MongoClient
+from bson.objectid import ObjectId
+from werkzeug.utils import secure_filename
+
+app = Flask(__name__, template_folder='static/templates')
+Bootstrap(app)
+
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+UPLOAD_FOLDER = os.path.join(BASE_DIR, 'static/uploads')
+ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg', 'gif'}
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+if not os.path.exists(UPLOAD_FOLDER):
+    os.makedirs(UPLOAD_FOLDER)
+
+def allowed_file(filename):
+    return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+client = MongoClient("mongodb://localhost:27017/")
+db = client["hotel_database"]
+collection = db["kamars"]
+
+@app.route('/uploads/<filename>')
+def uploaded_file(filename):
+    return send_from_directory(app.config['UPLOAD_FOLDER'], filename)
+
+@app.route('/')
+def index():
+    kamars = collection.find()
+    return render_template('index.html', kamars=kamars)
+
+@app.route('/add', methods=['GET', 'POST'])
+def add_file():
+    if request.method == 'POST':
+        nomor_kamar = request.form['nomor_kamar']
+        tipe_kamar = request.form['tipe_kamar']
+        harga_per_malam = request.form['harga_per_malam']
+        status = request.form['status']
+        
+        foto = None
+        if 'foto' in request.files:
+            file = request.files['foto']
+            if file and file.filename != '' and allowed_file(file.filename):
+                filename = secure_filename(file.filename)
+                file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+                foto = filename
+        
+        collection.insert_one({
+            'nomor_kamar': nomor_kamar,
+            'tipe_kamar': tipe_kamar,
+            'harga_per_malam': harga_per_malam,
+            'status': status,
+            'foto': foto
+        })
+        return redirect(url_for('index'))
+    return render_template('add.html')
+
+@app.route('/edit/<id>', methods=['GET', 'POST'])
+def edit_file(id):
+    kamar = collection.find_one({'_id': ObjectId(id)})
+    if request.method == 'POST':
+        nomor_kamar = request.form['nomor_kamar']
+        tipe_kamar = request.form['tipe_kamar']
+        harga_per_malam = request.form['harga_per_malam']
+        status = request.form['status']
+        
+        foto = kamar.get('foto')
+        if 'foto' in request.files:
+            file = request.files['foto']
+            if file and file.filename != '' and allowed_file(file.filename):
+                filename = secure_filename(file.filename)
+                file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+                foto = filename
+        
+        collection.update_one({'_id': ObjectId(id)}, {'$set': {
+            'nomor_kamar': nomor_kamar,
+            'tipe_kamar': tipe_kamar,
+            'harga_per_malam': harga_per_malam,
+            'status': status,
+            'foto': foto
+        }})
+        return redirect(url_for('index'))
+    return render_template('edit.html', kamar=kamar)
+
+@app.route('/delete/<id>')
+def delete_file(id):
+    collection.delete_one({'_id': ObjectId(id)})
+    return redirect(url_for('index'))
+
+if __name__ == '__main__':
+    app.run(debug=True)
--- UTS HOTEL/app.py
+++ UTS HOTEL/app.py
@@ -9,7 +9,7 @@ app.secret_key = 'secret123'
 
 app.config['MYSQL_HOST'] = 'localhost'
 app.config['MYSQL_USER'] = 'root'
-app.config['MYSQL_PASSWORD'] = 'Scendy@1904'
+app.config['MYSQL_PASSWORD'] = 'scendy1904'
 app.config['MYSQL_DB'] = 'crud_kamar_hotel'
 
 app.config['UPLOAD_FOLDER'] = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'uploads')
